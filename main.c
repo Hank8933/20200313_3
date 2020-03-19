1ffa5cdf0997dd6bffe81fd1fd3d8517ef7610ff
Hank8933 <48377489+Hank8933@users.noreply.github.com>
2020-03-19 21:03:42 +0800

    Updata

--- main.c
+++ main.c
@@ -1,105 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
-#define MAX 3
+#define MAX 12
 
-_Bool tryEveryResult(int *wA ,int a ,int b ,int c)
+int total = 0;
+int win = 0;
+
+int playerRound(int *stack)
 {
-    int flag = 1;
-    //printf("%d %d %d\n",a,b,c);
-    if (a == 0 && b == 0 && c == 0)
+    int c,n;
+    printf("請輸入移動哪[堆] [數量]: ");
+    scanf("%d%d",&c,&n);
+    if (n > 2 || n < 1 || stack[c-1] < n || c > 3 || c < 1)
     {
-        //printf("輸了\n");
-        return false;  //輸
+        printf("請重新輸入!\n");
+        return 0;
     }
-    if (a > 1)
-        if (tryEveryResult(wA ,a-2 ,b ,c))
-        {
-            flag = 0;
-        }
-    if (b > 1)
-        if (tryEveryResult(wA ,a ,b-2 ,c))
-        {
-            flag = 0;
-        }
-    if (c > 1)
-        if (tryEveryResult(wA ,a ,b ,c-2))
-        {
-            flag = 0;
-        }
-    if (a > 0)
-        if (tryEveryResult(wA ,a-1 ,b ,c))
-        {
-            flag = 0;
-        }
-    if (b > 0)
-        if (tryEveryResult(wA ,a ,b-1 ,c))
-        {
-            flag = 0;
-        }
-    if (c > 0)
-        if (tryEveryResult(wA ,a ,b ,c-1))
-        {
-            flag = 0;
-        }
-    if (flag) {return true;}
+    else
+    {
+        stack[c-1] -= n;
+        return 1;
+    }
 }
 
-void computerDo (int *stack)
+void check(int a,int b,int c,int player)
 {
-    int wA[2];
-    printf("電腦思考中...\n");
+    int i;
 
-    if (stack[0] > 1)
-        if (!(tryEveryResult(wA ,stack[0]-2 ,stack[1] ,stack[2])))
-        {
-            stack[0] -= 2;
-            return;
-        }
-    if (stack[1] > 1)
-        if (!(tryEveryResult(wA ,stack[0] ,stack[1]-2 ,stack[2])))
-        {
-            stack[1] -= 2;
-            return;
-        }
-    if (stack[2] > 1)
-        if (!(tryEveryResult(wA ,stack[0] ,stack[1] ,stack[2]-2)))
-        {
-            stack[2] -= 2;
-            return;
-        }
-    if (stack[0] > 0)
-        if (!(tryEveryResult(wA ,stack[0]-1 ,stack[1] ,stack[2])))
-        {
-            stack[0] -= 1;
-            return;
-        }
-    if (stack[1] > 0)
-        if (!(tryEveryResult(wA ,stack[0] ,stack[1]-1 ,stack[2])))
-        {
-            stack[1] -= 1;
-            return;
-        }
-    if (stack[2] > 0)
-        if (!(tryEveryResult(wA ,stack[0] ,stack[1] ,stack[2]-1)))
-        {
-            stack[2] -= 1;
-            return;
-        }
-    if (stack[0] != 0) stack[0] -= 1;
-    else if (stack[1] != 0) stack[1] -= 1;
-    else stack[2] -= 1;
-}
+    if(a == 0 && b == 0 && c == 0)
+    {
+        total++;
 
-void playerDo(int *stack)
-{
-    int which,amount;
-    printf("請選擇移除哪堆: ");
-    scanf("%d",&which);
-    printf("請選擇移除個數: ");
-    scanf("%d",&amount);
-    stack[which-1] -= amount;
+        if(player % 2 == 1)
+            win++;
+    }
+
+    for(i = 1; i <= 2 && a - i >= 0; i++) check(a - i, b, c, player + 1);
+    for(i = 1; i <= 2 && b - i >= 0; i++) check(a, b - i, c, player + 1);
+    for(i = 1; i <= 2 && c - i >= 0; i++) check(a, b, c - i, player + 1);
 }
 
 void showStone(int *stack)
@@ -115,24 +53,77 @@ void showStone(int *stack)
 int main()
 {
     srand(time(NULL));
-    int player;
-    int stack[3] = {};
+    int stack[3];
+    int player = 0; // 0 user 1 NPC
+    int choose, num, i;
+    double accuracy, max = -1;
     for (int i = 0; i < 3; i++) stack[i] = rand()%(MAX-2)+3;
-    printf("請選擇先後手 當先手輸入[1] 後手[0]: ");
+    printf("請選擇先後手 當先手輸入[0] 後手[1]: ");
     scanf("%d",&player);
-    printf("你選擇了 %s手\n",(player) ? "先" : "後");
-    showStone(stack);
+    printf("你選擇了 %s手\n",(player) ? "後" : "先");
+
     for (;;)
     {
-        if (player) playerDo(stack);
-        else computerDo(stack);
-        printf("%d %d %d\n",stack[0] ,stack[1] ,stack[2]);
         showStone(stack);
-        if (stack[0] == 0 && stack[1] == 0 && stack[2] == 0) break;
+
+        if(stack[0] == 0 && stack[1] == 0 && stack[2] == 0)
+        {
+            printf("Game Over %s贏了\n",(player & 1) ? "玩家" : "電腦");
+            break;
+        }
+        if(player % 2 == 0) while(!playerRound(stack));
+        else  //computer
+        {
+            printf("電腦思考中...\n");
+            max = -1;
+            for(i = 1; i <= 2 && stack[0] - i >= 0; i++)
+            {
+                win = total = 0;
+                check(stack[0] - i, stack[1], stack[2], 1);
+                accuracy = (double)win / total;
+                //printf("勝率: %f\n",accuracy);
+                if(max < accuracy)
+                {
+                    choose = 1;
+                    num = i;
+                    max = accuracy;
+                }
+            }
+
+            for(i = 1; i <= 2 && stack[1] - i >= 0; i++)
+            {
+                win = total = 0;
+                check(stack[0], stack[1] - i, stack[2], 1);
+                accuracy = (double)win / total;
+                //printf("勝率: %f\n",accuracy);
+                if(max < accuracy)
+                {
+                    choose = 2;
+                    num = i;
+                    max = accuracy;
+                }
+            }
+
+            for(i = 1; i <= 2 && stack[2] - i >= 0; i++)
+            {
+                win = total = 0;
+                check(stack[0], stack[1], stack[2] - i, 1);
+                accuracy = (double)win / total;
+                //printf("勝率: %f\n",accuracy);
+                if(max < accuracy)
+                {
+                    choose = 3;
+                    num = i;
+                    max = accuracy;
+                }
+            }
+
+            if(choose == 1) stack[0] -= num;
+            if(choose == 2) stack[1] -= num;
+            if(choose == 3) stack[2] -= num;
+        }
         player++;
-        player %= 2;
     }
-    printf("Game Over %s贏了\n",(player) ? "玩家" : "電腦");
     system("pause");
     return 0;
 }
